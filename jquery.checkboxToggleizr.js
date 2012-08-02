9cd8e75254ddea0269c0000d54a4baae04f9b35b
Alexander Bogomolov <alexander@bogomolov.de>
2012-08-02 08:40:25 +0200

    Basic function

---
+++ jquery.checkboxToggleizr.js
@@ -0,0 +1,41 @@
+/**
+ * Check or uncheck all checkboxes bei one checkbox referenced by data-reference.
+ * data-reference attribute of "check all checkbox" must be the name attribute of the checkboxes.
+ *
+ * Author:    Alexander Bogomolov
+ * Homepage:  http://www.bogomolov.de/
+ * Version:   1.0
+ */
+;(function($) {
+
+    $.checkboxToggleizr = function(element, options) {
+      this.defaults = {};
+      this.$element = $(element);
+
+      this.init = function() {
+
+        this.$element.click(function(e) {
+          var self = $(this);
+          var reference = self.data('reference');
+
+          if(self.is(':checked')) {
+            $(':checkbox[name="'+ reference +'"]').attr('checked', 'checked');
+          }
+          else {
+            $(':checkbox[name="'+ reference +'"]').removeAttr('checked');
+          }
+        });
+      };
+
+      this.init();
+      return this;
+    };
+
+
+
+    return $.fn.checkboxToggleizr = function(options) {
+      return this.each(function() {
+        return new $.checkboxToggleizr(this, options);
+      })
+    }
+})(jQuery);
